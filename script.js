9fced1ef190ec2db0f7241ea4970a3c354a24228
Alex Ermolenko <alex.ermolenko1@gmail.com>
2022-04-17 12:40:52 +0300

    commit

--- script.js
+++ script.js
@@ -11,19 +11,27 @@ const input = $(".input");
 const select = $(".select");
 const DELAY = 150;
 
-function transform(width, height, radius, color = "") {
+function transform(width, height, radius) {
   figure.fadeOut(DELAY);
   setTimeout (() => {
     figure.width(`${width}px`);
     figure.height(`${height}px`);
     figure.css("border-radius", `${radius}%`);
-    if (color) figure.css("background-color", color);
   }, DELAY);
   figure.fadeIn(DELAY);
 } 
 
+function changeBackground(color) {
+  figure.fadeOut(DELAY);
+  setTimeout (() => {
+    figure.css("background-color", color);
+  }, DELAY);
+  figure.fadeIn(DELAY);
+} 
+
+
 input.change(() => {
-  transform(300, 300, 0, input.val());
+  changeBackground(input.val());
 });
 
 select.change(() => {
